168a42e2656f8d1e0e9b7228ac149480cbada12c
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-07-23 00:03:37 +0300

    add testing that next number is proper

--- tc_task.rb
+++ tc_task.rb
@@ -7,8 +7,18 @@ class TestTask < Test::Unit::TestCase
     @sequence = Sequence.new
   end
 
-  def test_proverka
-    assert_equal("1", @sequence.init_number)
+  def test_next_number_is_proper
+    assert_equal("11", @sequence.next_number("1"))
+    assert_equal("21", @sequence.next_number("11"))
+    assert_equal("1211", @sequence.next_number("21"))
+    assert_equal("111221", @sequence.next_number("1211"))
+    assert_equal("312211", @sequence.next_number("111221"))
+    assert_equal("13112221", @sequence.next_number("312211"))
+    assert_equal("1113213211", @sequence.next_number("13112221"))
+  end
+
+  def test_sequence_is_proper
+    assert_equal("1\n11\n21\n1211\n111221\n312211\n13112221\n1113213211", @sequence.print_sequence(8))
   end
 
 end
